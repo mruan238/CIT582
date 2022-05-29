55be6ac6d139047d88d805de2b185f5d55dad515
Ryan <mac@MacBook-Pro.local>
2022-05-29 23:57:23 +0800

    Update sign.py

--- sign.py
+++ sign.py
@@ -8,12 +8,13 @@ from hashlib import sha256
 def sign(m):
 	#generate public key
 	#Your code here
-	private_key = keys.gen_private_key(curve.secp256k1)
-	public_key = keys.get_public_key(private_key, curve.secp256k1)
+	private_key = keys.gen_private_key(secp256k1)
+	public_key = keys.get_public_key(private_key, secp256k1)
 	#generate signature
 	#Your code here
-	r, s = ecdsa.sign(m, private_key, hashfunc=sha256)
+	r, s = ecdsa.sign(m, private_key, curve=secp256k1, hashfunc=sha256)
 	assert isinstance( public_key, point.Point )
 	assert isinstance( r, int )
 	assert isinstance( s, int )
 	return( public_key, [r,s] )
+
