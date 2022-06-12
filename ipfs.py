666ae96cf604d75145004db5ca9fbaee080dd987
Ryan <mac@MacBook-Pro.local>
2022-06-12 23:49:05 +0800

    Update ipfs.py

--- ipfs.py
+++ ipfs.py
@@ -5,7 +5,7 @@ def pin_to_ipfs(data):
 	assert isinstance(data,dict), f"Error pin_to_ipfs expects a dictionary"
 	#YOUR CODE HERE
 	response = requests.post('https://ipfs.infura.io:5001/api/v0/add', files=data)
-	cid = response.json()['Hash']
+	cid = response.json()
 	return cid
 
 def get_from_ipfs(cid,content_type="json"):
