9fb998c818e5a17079b5062d77678c1aee4420c4
Ryan <mac@MacBook-Pro.local>
2022-06-25 14:19:29 +0800

    Update num_BTC.py

--- num_BTC.py
+++ num_BTC.py
@@ -1,7 +1,14 @@
 import math
 
 def num_BTC(b):
-    c = float(b * 50)
-    return c
+    ntokens = 50
+    reward = 50
+    blocks = 1
+    while (blocks < b):
+        if (blocks % 210_000==0):
+            reward = reward / 2
+        ntokens = ntokens + reward
+        blocks = blocks + 1
 
+    return float(ntokens)
 
