511a2483f9cf9f1aa9e41b818e00cb07832669c4
Ryan <mac@MacBook-Pro.local>
2022-07-16 20:39:03 +0800

    Update verfication_endpoint.py

--- verfication_endpoint.py
+++ verfication_endpoint.py
@@ -10,47 +10,24 @@ app.url_map.strict_slashes = False
 
 @app.route('/verify', methods=['GET','POST'])
 def verify():
-    result = False
-    content = request.get_json(silent=True)    
-    sk = content['sig']
-    payload = content['payload']
-    platform = content['payload']['platform']
-    message = json.dumps(payload)
-    pk = payload['pk']
-
+    content = request.get_json(silent=True)
+    platform = content['payload']['platform'] 
+    message = content['payload']['message'] 
+    pk = content['payload']['pk'] 
+    sig = content['sig']
+    payload = json.dumps(content['payload'])
+    response = False
     
-    #1. Verifying an endpoint for verifying signatures for ethereum
-    if platform == "Ethereum":
+    if platform=='Ethereum':
+        eth_encoded_msg = eth_account.messages.encode_defunct(text=payload)
+        if eth_account.Account.recover_message(eth_encoded_msg,signature=sig) == pk:
+            response = True
 
-        #eth_account.Account.enable_unaudited_hdwallet_features()
-        #acct, mnemonic = eth_account.Account.create_with_mnemonic()
+    if platform=='Algorand':
+        if algosdk.util.verify_bytes(payload.encode('utf-8'),sig,pk):
+            response = True
 
-        #eth_pk = acct.address
-        #eth_sk = acct.key
-
-        #payload = "Sign this!"
-
-        eth_encoded_msg = eth_account.messages.encode_defunct(text=message)
-        #eth_sig_obj = eth_account.Account.sign_message(eth_encoded_msg,sk)
-        #print( eth_sig_obj.messageHash )
-        recovered_pk = eth_account.Account.recover_message(eth_encoded_msg,signature=sk)
-        if(recovered_pk ==pk):
-            result = True
-            print( "Eth sig verifies!" )    
-    
-    #2. Verifying an endpoint for verifying signatures for Algorand
-    elif platform == "Algorand":
-        result = algosdk.util.verify_bytes(message.encode('utf-8'),sk,pk)
-        if(result):
-            print( "Algo sig verifies!" )
-    
-    #3. Check for invalid input
-    else:
-        print("invalid input")
-
-    #Check if signature is valid
-    #result = True #Should only be true if signature validates
-    return jsonify(result)
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(port='5002')
