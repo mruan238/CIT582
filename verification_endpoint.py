d8af0b5c62d16646e43db79a8054dcc106559542
Ryan <mac@MacBook-Pro.local>
2022-07-16 21:40:33 +0800

    Update verification_endpoint.py

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -10,42 +10,23 @@ app.url_map.strict_slashes = False
 
 @app.route('/verify', methods=['GET','POST'])
 def verify():
-    content = request.get_json(silent=True)
-    print("Working ETH",content)
-    #Verify Etherium Signature
-    result = False #Should only be true if signature validates
-    if content.payload.platform=="Ethereum":
-      
-        eth_account.Account.enable_unaudited_hdwallet_features()
-        acct, mnemonic = eth_account.Account.create_with_mnemonic()
-
-        eth_pk = acct.address
-        eth_sk = acct.key
-
-        payload = content.payload
-
-        eth_encoded_msg = eth_account.messages.encode_defunct(text=payload)
-        eth_sig_obj = eth_account.Account.sign_message(eth_encoded_msg,eth_sk)
-        
-        # print( eth_sig_obj.messageHash )  
-        if eth_account.Account.recover_message(eth_encoded_msg,signature=eth_sig_obj.signature.hex()) == eth_pk:
-            result=True
-        
-    #Verify Algorand Signature
-    elif content.payload.platform=="Algorand":
-        payload = content.payload
-
-        algo_sk, algo_pk = algosdk.account.generate_account()
-        algo_sig_str = algosdk.util.sign_bytes(payload.encode('utf-8'),algo_sk)
-
-        if algosdk.util.verify_bytes(payload.encode('utf-8'),algo_sig_str,algo_pk):
-            print( "Algo sig verifies!" )
-            result=True
-        
+    content = request.get_json(silent=True, force=True)
+    #Check if signature is valid
+    print(content)
+    if content == None:
+        return jsonify("No json data is sent.")
+    sig = content.get('sig')
+    payload = content.get('payload')
+    message = payload.get('message')
+    pk = payload.get('pk')
+    platform = payload.get('platform')
+    if platform == "Ethereum":
+        encoded_msg = eth_account.messages.encode_defunct(text=json.dumps(payload))
+        result = eth_account.Account.recover_message(encoded_msg,signature=sig) == pk
     else:
-        result = False
-    
+        result = algosdk.util.verify_bytes(json.dumps(payload).encode('utf-8'), sig, pk)
     return jsonify(result)
+
 
 if __name__ == '__main__':
     app.run(port='5002')
