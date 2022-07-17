f1963c2eef7e6223494f763a5f3dbeb350bdb5ed
Ryan <mac@MacBook-Pro.local>
2022-07-17 11:02:59 +0800

    Update order_book.py

--- order_book.py
+++ order_book.py
@@ -8,40 +8,48 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+
 def process_order(order):
     #Your code here
-    order["filled"]=None
-    order["counterparty_id"]=None
     order_obj = Order( sender_pk=order['sender_pk'],receiver_pk=order['receiver_pk'], buy_currency=order['buy_currency'], sell_currency=order['sell_currency'], buy_amount=order['buy_amount'], sell_amount=order['sell_amount'] )
     session.add(order_obj)
     session.commit()
-    lastInserted=session.query(Order).all()[len(session.query(Order).all())-1]
-    queryResults = session.query(Order).all()
-    for existingOrder in queryResults:
-        if (existingOrder.filled==None and existingOrder.buy_currency==order["sell_currency"] and existingOrder.sell_currency==order["buy_currency"] and (existingOrder.sell_amount/existingOrder.buy_amount)>=(order["buy_amount"]/order["sell_amount"])):
-            existingOrder.filled=datetime.now()
-            lastInserted.filled=datetime.now()
-            lastInserted.counterparty_id=existingOrder.id
-            existingOrder.counterparty_id=lastInserted.id
-            # print(existingOrder.filled, existingOrder.counterparty_id, )
-            if(existingOrder.sell_amount<lastInserted.buy_amount):
-                nOrder = {}
-                nOrder["created_by"]=lastInserted.id
-                nOrder['sender_pk'] = lastInserted.sender_pk
-                nOrder['receiver_pk'] = lastInserted.receiver_pk
-                nOrder['buy_currency'] = lastInserted.buy_currency
-                nOrder['sell_currency'] = lastInserted.sell_currency
-                nOrder['sell_amount'] = lastInserted.sell_amount
-                nOrder['buy_amount'] = (lastInserted.sell_amount-existingOrder.sell_amount)
-                process_order(nOrder)
-            if(lastInserted.sell_amount<existingOrder.buy_amount):
-                nOrder = {}
-                nOrder["created_by"]=existingOrder.id
-                nOrder['sender_pk'] = existingOrder.sender_pk
-                nOrder['receiver_pk'] = existingOrder.receiver_pk
-                nOrder['buy_currency'] = existingOrder.buy_currency
-                nOrder['sell_currency'] = existingOrder.sell_currency
-                nOrder['sell_amount'] = existingOrder.sell_amount
-                nOrder['buy_amount'] = (existingOrder.sell_amount-lastInserted.sell_amount)
-                process_order(nOrder)
-            break
+
+    matched_order = session.query(Order).filter(Order.filled==None,\
+                                       Order.buy_currency == order_obj.sell_currency,\
+                                       Order.sell_currency == order_obj.buy_currency,\
+                                       Order.sell_amount/Order.buy_amount >= order_obj.buy_amount/order_obj.sell_amount).first()
+
+    if matched_order != None:
+        matched_order.filled = datetime.now()
+        order_obj.filled = matched_order.filled
+        
+        matched_order.counterparty_id = order_obj.id
+        order_obj.counterparty_id = matched_order.id
+
+            
+        if order_obj.buy_amount > matched_order.sell_amount:
+                new_order = Order(sender_pk = order_obj.sender_pk,receiver_pk = order_obj.receiver_pk, \
+                                  buy_currency = order_obj.buy_currency, sell_currency = order_obj.sell_currency, \
+                                  buy_amount = order_obj.buy_amount - matched_order.sell_amount, \
+                                  sell_amount = (order_obj.buy_amount - matched_order.sell_amount)* order_obj.sell_amount / order_obj.buy_amount,\
+                                  creator_id = order_obj.id)
+                print("partially filled, new_order.buy_amount > matched_order.sell amount, creator_id =", new_order.creator_id)
+                session.add(new_order)
+                session.commit()
+
+                        
+
+                    
+        if matched_order.buy_amount > order_obj.sell_amount:
+                new_order = Order(sender_pk = matched_order.sender_pk,receiver_pk = matched_order.receiver_pk, \
+                                  buy_currency =matched_order.buy_currency, sell_currency = matched_order.sell_currency, \
+                                  buy_amount = matched_order.buy_amount - order_obj.sell_amount, \
+                                  sell_amount= (matched_order.buy_amount - order_obj.sell_amount) * matched_order.sell_amount / matched_order.buy_amount,\
+                                  creator_id = matched_order.id)
+                print("partially filled, matched_order.buy_amount>new_order.sell_amount, creator_id =", new_order.creator_id)
+                
+                session.add(new_order)
+                session.commit()
+
+    pass
