40060a9d723a50b549950387aa08d9cd52e81871
Ryan <mac@MacBook-Pro.local>
2022-08-16 02:17:31 +0800

    Update gen_keys.py

--- gen_keys.py
+++ gen_keys.py
@@ -10,3 +10,12 @@ sender_pk = mnemonic.to_public_key(mnemonic_secret)
 print("sender_sk: ", sender_sk)
 print("sender_pk: ", sender_pk)
 
+
+w3 = Web3()
+w3.eth.account.enable_unaudited_hdwallet_features()
+acct,mnemonic_secret = w3.eth.account.create_with_mnemonic()
+acct = w3.eth.account.from_mnemonic(mnemonic_secret)
+eth_pk = acct._address
+eth_sk = acct._private_key
+print("eth_sk: ", eth_sk)
+print("eth_pk: ", eth_pk)
